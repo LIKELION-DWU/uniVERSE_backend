1772334caad66c03ad76794e66f12292f5ec779b
hong <trre1827151@gmail.com>
2023-09-15 17:52:42 +0900

    [Feat] 개발환경세팅

--- festival2023/contents/views.py
+++ festival2023/contents/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib import admin
+from .models import College, Student, Booth, Book
 
-# Create your views here.
+
+class College_list(admin.ModelAdmin):
+    list_display = '__all__'
+admin.site.register(College, College_list)
+
+class Student_list(admin.ModelAdmin):
+    list_display = ('id', 'name', 'customer', 'product')
+admin.site.register(Student, Student_list)
+
+class Booth_list(admin.ModelAdmin):
+    list_display = ('id', 'num', 'customer')
+admin.site.register(Booth, Booth_list)
+
+class Book_list(admin.ModelAdmin):
+    list_display = ('id', 'num', 'customer')
+admin.site.register(Book, Card_list)
