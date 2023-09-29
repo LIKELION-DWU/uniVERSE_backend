0891deb82d7e6f0065d68d744c631f941346e373
hong <trre1827151@gmail.com>
2023-09-29 21:37:06 +0900

    배포 url 적용

--- festival2023/festival2023/settings.py
+++ festival2023/festival2023/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # static -files 경로 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
